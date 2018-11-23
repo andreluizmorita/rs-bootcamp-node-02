ad455c4aa50a00d44ea1f92c577c791db7da5d25
andremorita <andreluizmorita@gmail.com>
2018-11-23 17:31:06 -0200

    Logout

--- src/routes.js
+++ src/routes.js
@@ -10,13 +10,15 @@ const guestMiddleware = require('./app/middlewares/guest')
 const UserController = require('./app/controllers/UserController')
 const SessionController = require('./app/controllers/SessionController')
 
-routes.use('/app', authMiddleware)
-
 routes.get('/', guestMiddleware, SessionController.create)
 routes.post('/signin', SessionController.store)
 
 routes.get('/signup', guestMiddleware, UserController.create)
 routes.post('/signup', upload.single('avatar'), UserController.store)
+
+routes.use('/app', authMiddleware)
+
+routes.get('/app/logout', SessionController.destroy)
 
 routes.get('/app/dashboard', (req, res) => {
   console.log(req.session.user)
